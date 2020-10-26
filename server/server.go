94a2120059e11a6b822bacd45de8ea555f612139
ssubedir <ssubedir@gmail.com>
2020-10-25 22:59:23 -0400

    Server update

--- server/server.go
+++ server/server.go
@@ -3,13 +3,15 @@ package server
 import (
 	"context"
 	"database/sql"
+	"encoding/json"
 	"fmt"
-	"html/template"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"time"
 
+	gohandlers "github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	_ "github.com/joho/godotenv/autoload"
 )
@@ -22,13 +24,24 @@ type BServer struct {
 func NewBServer() BServer {
 	sm := mux.NewRouter()
 	getR := sm.Methods(http.MethodGet).Subrouter()
-	getR.HandleFunc("/", Index)
 	getR.HandleFunc("/status", Status)
 	sm.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("template/static")))) // serve static files
 
+	ch := gohandlers.CORS(
+		gohandlers.AllowedHeaders([]string{"Authorization", "Accept", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token"}),
+		gohandlers.AllowedOrigins([]string{"*"}),
+		gohandlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"}),
+		gohandlers.AllowCredentials(),
+	)
+	// Api
+
+	// Get all cpu
+
+	getR.HandleFunc("/api/amazon", Amazon)
+
 	s := http.Server{
 		Addr:         ":" + os.Getenv("SERVER_PORT"), // configure the bind address
-		Handler:      sm,                             // set the default handler
+		Handler:      ch(sm),                         // set the default handler
 		ReadTimeout:  5 * time.Second,                // max time to read request from the client
 		WriteTimeout: 10 * time.Second,               // max time to write response to the client
 		IdleTimeout:  120 * time.Second,              // max time for connections using TCP Keep-Alive
@@ -63,13 +76,25 @@ func Status(w http.ResponseWriter, r *http.Request) {
 }
 
 // Fetch Prices of all cpu
-func Index(w http.ResponseWriter, r *http.Request) {
+func Amazon(w http.ResponseWriter, r *http.Request) {
+
+	uq := r.URL.Query()
+	loc := uq.Get("location")
+	locid := 0
+
+	if loc == "us" {
+		locid = 1
+	} else if loc == "ca" {
+		locid = 2
+	} else {
+		locid = 1
+	}
 
 	q := `SELECT _price, _time
 	FROM hw_cpu hc 
 	LEFT JOIN hw_cpu_amazon hca 
 	ON hc.AmazonId = hca. _asin
-	WHERE hc.AmazonId = (SELECT hc2.AmazonId from hw_cpu hc2 where hc2.Name = ?) AND hca. _country = 1 
+	WHERE hc.AmazonId = (SELECT hc2.AmazonId from hw_cpu hc2 where hc2.Name = ?) AND hca. _country = ? 
 	ORDER BY hca. _time DESC
 	LIMIT 1`
 
@@ -99,7 +124,7 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 	for i, b := range data.Cpus {
 
-		rows, _ := maria.Query(q, b.Name)
+		rows, _ := maria.Query(q, b.Name, locid)
 
 		for rows.Next() {
 
@@ -111,8 +136,10 @@ func Index(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	t, _ := template.ParseFiles("template/index.html")
-	t.Execute(w, data)
+	w.Header().Add("Content-Type", "application/json")
+
+	ToJSON(data, w)
+
 }
 
 type CPUS struct {
@@ -125,9 +152,15 @@ type PD struct {
 }
 
 type CPU struct {
-	Id       int       `db:"Id"`
-	Name     string    `db:"Name"`
-	AmazonId string    `db:"AmazonId"`
-	Price    float64   `db:"_price""`
-	Time     time.Time `db:"_time""`
+	Id       int       `db:"Id" json:"-"`
+	Name     string    `db:"Name" json:"Name,omitempty"`
+	AmazonId string    `db:"AmazonId" json:"AmazonId,omitempty"`
+	Price    float64   `db:"_price" json:"Price,omitempty"`
+	Time     time.Time `db:"_time" json:"Time,omitempty"`
+}
+
+// ToJSON serializes the given interface into a string based JSON format
+func ToJSON(i interface{}, w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(i)
 }
